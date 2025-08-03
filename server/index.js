f2a40f8f7e1d6f50938464e004c93f374e75ed2f
SoremOne <soremkrs@gmail.com>
2025-08-03 16:26:48 +0100

    cookies fixing check

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(cors({ origin: process.env.ORIGIN_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.set('trust proxy', 1); 
+app.set('trust proxy', true); 
 
 app.use((req, res, next) => {
   console.log('Request protocol:', req.protocol);
